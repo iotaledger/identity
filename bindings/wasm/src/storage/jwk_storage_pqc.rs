d2bca6d1ba354cf17964e4b63f7fa51a19a5524b
AleCla97 <alessio.claudio97@gmail.com>
2025-06-20 12:13:17 +0200

    Refactor async functions to return JsValue directly and simplify promise handling in JwkStoragePQ implementation

--- bindings/wasm/src/storage/jwk_storage_pqc.rs
+++ bindings/wasm/src/storage/jwk_storage_pqc.rs
@@ -9,26 +9,20 @@ use identity_iota::storage::KeyStorageResult;
 use identity_iota::storage::KeyType;
 use identity_iota::verification::jwk::PostQuantumJwk;
 use wasm_bindgen::prelude::*;
-use crate::error::JsValueResult;
-use js_sys::Promise;
 use identity_iota::storage::KeyStorageErrorKind;
 use identity_iota::verification::jose::jws::JwsAlgorithm;
 use identity_iota::storage::KeyStorageError;
-use wasm_bindgen_futures::JsFuture;
-use super::jwk_storage::PromiseJwkGenOutput;
 use crate::jose::WasmJwk;
 use js_sys::Uint8Array;
-
-use crate::common::PromiseUint8Array;
 
 #[wasm_bindgen]
 extern "C" {
 
   #[wasm_bindgen(method, js_name = generatePQKey)]
-  pub fn _generate_pq_key(this: &WasmJwkStorage, key_type: String, alg: String) -> PromiseJwkGenOutput;
+  pub async fn _generate_pq_key(this: &WasmJwkStorage, key_type: String, alg: String) -> JsValue;
 
   #[wasm_bindgen(method, js_name = signPQ)]
-  pub fn _pq_sign(this: &WasmJwkStorage, key_id: String, data: Vec<u8>, public_key: WasmJwk, ctx: Option<&[u8]>) -> PromiseUint8Array;
+  pub async fn _pq_sign(this: &WasmJwkStorage, key_id: String, data: Vec<u8>, public_key: WasmJwk, ctx: Option<&[u8]>) -> JsValue;
 
 }
 
@@ -36,21 +30,22 @@ extern "C" {
 #[async_trait::async_trait(?Send)]
 impl JwkStoragePQ for WasmJwkStorage {
   async fn generate_pq_key(&self, key_type: KeyType, alg: JwsAlgorithm) -> KeyStorageResult<JwkGenOutput> {
-    let promise: Promise = Promise::resolve(&WasmJwkStorage::_generate_pq_key(self, key_type.into(), alg.name().to_owned()));
-    let result: JsValueResult = JsFuture::from(promise).await.into();
-    result.into()
+    WasmJwkStorage::_generate_pq_key(self, key_type.into(), alg.name().to_owned())
+    .await
+    .into_serde()
+    .map_err(|e| KeyStorageError::new(KeyStorageErrorKind::SerializationError).with_source(e))
   }
 
   async fn pq_sign(&self, key_id: &KeyId, data: &[u8], public_key: &PostQuantumJwk, ctx: Option<&[u8]>) -> KeyStorageResult<Vec<u8>> {
-    let promise: Promise = Promise::resolve(&WasmJwkStorage::_pq_sign(
+    let value = WasmJwkStorage::_pq_sign(
       self,
       key_id.clone().into(),
       data.to_owned(),
       WasmJwk(public_key.clone().into()),
       ctx
-    ));
-    let result: JsValueResult = JsFuture::from(promise).await.into();
-    result.to_key_storage_error().map(uint8array_to_bytes)?
+    ).await;
+
+    uint8array_to_bytes(value)
   }
 
 }

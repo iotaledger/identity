0ef89f7c2b817f6d2164296771162ccdcf25058e
Christof Gerritsma <christof.gerritsma@iota.org>
2025-11-07 14:56:44 +0100

    feat: Update to iota v1.10.0 (#1737)
    * pin iota to "v1.10.0-rc" and product-core to upstream-merge feature branch
    
    Summary:
    - [ ] tokio version update to: -
    - [ ] fastcrypto version update to rev = "-"
    - [ ] identity_wasm: new peerDep. version for @iota/iota-sdk: _
    - [x] pin all product-core.git dependencies to `branch = "feat/iota-v1-10-0-rc-upstream-merge"`
    - [x] pin all iota.git dependencies to `tag = "v1.10.0-rc"`
    - [x] pin all notarization.git dependencies to `branch = "feat/iota-v1-10-0"`
    
    * Update dependencies to iota v1.10.0 and product-core v0.8.5
    
    - pin all iota.git dependencies to `tag = "v1.10.0"`
    - pin all product-core.git dependencies to `tag = "v0.8.5"`
    - pin all notarization.git dependencies to `tag = "v0.1.9"`
    - identity_wasm: @iota/iota-interaction-ts dependency stays with "^0.8.0"
    
    * Fix doubled quotes error in cargo.toml files
    
    * clippy lints
    
    * fmt
    
    ---------
    
    Co-authored-by: Enrico Marconi <enrico.marconi@iota.org>

--- identity_credential/src/validator/options.rs
+++ identity_credential/src/validator/options.rs
@@ -8,6 +8,7 @@ use serde::Serialize;
 /// [`credentialStatus`](https://www.w3.org/TR/vc-data-model/#status).
 #[derive(Debug, Clone, Copy, PartialEq, Eq, serde_repr::Serialize_repr, serde_repr::Deserialize_repr)]
 #[repr(u8)]
+#[derive(Default)]
 pub enum StatusCheck {
   /// Validate the status if supported, reject any unsupported
   /// [`credentialStatus`](https://www.w3.org/TR/vc-data-model/#status) types.
@@ -15,6 +16,7 @@ pub enum StatusCheck {
   /// Only `RevocationBitmap2022` is currently supported.
   ///
   /// This is the default.
+  #[default]
   Strict = 0,
   /// Validate the status if supported, skip any unsupported
   /// [`credentialStatus`](https://www.w3.org/TR/vc-data-model/#status) types.
@@ -23,33 +25,23 @@ pub enum StatusCheck {
   SkipAll = 2,
 }
 
-impl Default for StatusCheck {
-  fn default() -> Self {
-    Self::Strict
-  }
-}
-
 /// Declares how credential subjects must relate to the presentation holder during validation.
 ///
 /// See also the [Subject-Holder Relationship](https://www.w3.org/TR/vc-data-model/#subject-holder-relationships) section of the specification.
 // Need to use serde_repr to make this work with duck typed interfaces in the Wasm bindings.
 #[derive(Debug, Clone, Copy, serde_repr::Serialize_repr, serde_repr::Deserialize_repr)]
 #[repr(u8)]
+#[derive(Default)]
 pub enum SubjectHolderRelationship {
   /// The holder must always match the subject on all credentials, regardless of their [`nonTransferable`](https://www.w3.org/TR/vc-data-model/#nontransferable-property) property.
   /// This is the variant returned by [Self::default](Self::default()) and the default used in
   /// [`crate::validator::JwtPresentationValidationOptions`].
+  #[default]
   AlwaysSubject = 0,
   /// The holder must match the subject only for credentials where the [`nonTransferable`](https://www.w3.org/TR/vc-data-model/#nontransferable-property) property is `true`.
   SubjectOnNonTransferable = 1,
   /// Declares that the subject is not required to have any kind of relationship to the holder.
   Any = 2,
-}
-
-impl Default for SubjectHolderRelationship {
-  fn default() -> Self {
-    Self::AlwaysSubject
-  }
 }
 
 /// Declares when validation should return if an error occurs.

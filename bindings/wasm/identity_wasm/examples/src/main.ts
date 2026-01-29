06e02f04800d63f433c5f09d982d2f844f94084e
Enrico Marconi <enrico.marconi@iota.org>
2026-01-29 14:42:50 +0100

    update example names

--- bindings/wasm/identity_wasm/examples/src/main.ts
+++ bindings/wasm/identity_wasm/examples/src/main.ts
@@ -23,7 +23,7 @@ import { zkp } from "./1_advanced/8_zkp";
 import { zkp_revocation } from "./1_advanced/9_zkp_revocation";
 import { hybrid } from "./1_advanced/hybrid";
 import { pq } from "./1_advanced/pq";
-import { sdJwtV2 } from "./1_advanced/sd_jwt_v2";
+import { sdJwtV2 } from "./1_advanced/6_sd_jwt_v2";
 
 export async function main(example?: string) {
     // Extract example name.
@@ -59,6 +59,8 @@ export async function main(example?: string) {
             return await domainLinkage();
         case "6_sd_jwt":
             return await sdJwt();
+        case "6_sd_jwt_v2":
+            return await sdJwtV2();
         case "7_status_list_2021":
             return await statusList2021();
         case "8_zkp":
@@ -77,8 +79,6 @@ export async function main(example?: string) {
             return await iotaKeytoolIntegration();
         case "13_linked_verifiable_presentation":
             return await linkedVp();
-        case "sd_jwt_v2":
-            return await sdJwtV2();
         default:
             throw "Unknown example name: '" + argument + "'";
     }

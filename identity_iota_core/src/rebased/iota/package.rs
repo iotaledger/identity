cb96a98894b11271dc6b591b3f40067677476fa3
Enrico Marconi <enrico.marconi@iota.org>
2026-03-02 16:51:45 +0100

    track devnet new package ID

--- identity_iota_core/src/rebased/iota/package.rs
+++ identity_iota_core/src/rebased/iota/package.rs
@@ -43,11 +43,7 @@ static IOTA_IDENTITY_PACKAGE_REGISTRY: LazyLock<RwLock<PackageRegistry>> = LazyL
     );
     registry.insert_env(
       Env::new_with_alias("e678123a", "devnet"),
-      vec![
-        object_id!("0xe6fa03d273131066036f1d2d4c3d919b9abbca93910769f26a924c7a01811103"),
-        object_id!("0x6a976d3da90db5d27f8a0c13b3268a37e582b455cfc7bf72d6461f6e8f668823"),
-        object_id!("0xc04befdea27caa7e277f0b738bfcd29fc463cd2a5885ae7f0a9fd3e2d635a8b8"),
-      ],
+      vec![object_id!("0x8896ab04fe24c044c54925df3f8a7c383a8d1d6f6bbb95d1c57cfa94c75e520d")],
     );
 
     registry
